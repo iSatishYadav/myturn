83f47efe70bf083d9020e3669ac3db33bccb06ba
Satish Yadav <monstersatish@live.com>
2019-09-04 21:20:50 +0530

    sw adder

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 (function() {
+    window.addEventListener('load', e => {        
+        registerSW(); 
+      });
 
+      async function registerSW() {
+          if ('serviceWorker' in navigator) {
+              try {
+                  await navigator.serviceWorker.register('./sw.js');
+                } catch (e) {
+                    alert('ServiceWorker registration failed. Sorry about that.');
+                }
+            } else {
+                document.querySelector('.alert').removeAttribute('hidden');
+            }
+        }
+      
     $(document).ready(function() {
       
         function calculateWinner() {
---
+++ sw.js
@@ -0,0 +1,54 @@
+// Hey there! This is an over-simplified ServiceWorker for a tutorial.
+// For any real apps, please use workboxjs.org or similar
+// If you do want to use this, you'll need to update the file manually for every change to trigger an update
+// Last modified: 2018-04-25 12:58PT
+
+const cacheName = 'pwa-myturn-v1';
+//<link rel="stylesheet" href="css/bootstrap.min.css" />
+{/* <link rel="stylesheet" href="css/main.css"/> */}
+{/* <script src="index.js" ></script> */}
+{/* <script src="js/jquery-3.4.1.min.js"></script>         */}
+
+
+const staticAssets = ['./', './index.html', './index.js', './css/main.css', './css/bootstrap.min.css', './js/jquery-3.4.1.min.js'];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+// Optional: clents.claim() makes the service worker take over the current page
+// instead of waiting until next load. Useful if you have used SW to prefetch content
+// that's needed on other routes. But potentially dangerous as you are still running the
+// previous version of the app, but with new resources.
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (/.*(json)$/.test(req.url)) {
+    event.respondWith(networkFirst(req));
+  } else {
+    event.respondWith(cacheFirst(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || networkFirst(req);
+}
+
+async function networkFirst(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse;
+  }
+}
